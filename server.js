e370b320687d13dca47811c7590880f8373d5da2
MikChanna <channa.mik@gmail.com>
2020-09-23 19:55:45 -0400

    created api and html route files

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
 });
 
 // routes
-app.use(require("./public/api.js"));
+app.use(require("./routes/api-route.js"));
+app.use(require("./routes/html-route.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
