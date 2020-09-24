f1c8518fadd1c1769be0587b29e8d807b47d1ea6
MikChanna <channa.mik@gmail.com>
2020-09-23 20:11:20 -0400

    added html routes for files

---
+++ routes/html-route.js
@@ -0,0 +1,16 @@
+// requiring path so wer can use relative routes to our html files
+const path = require("path");
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+  app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+};
