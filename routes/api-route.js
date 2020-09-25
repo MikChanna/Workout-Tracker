69c650232702ab79d16ee04dfb2a4cb07ce590a5
MikChanna <channa.mik@gmail.com>
2020-09-25 16:29:42 -0400

    added update api for workout

--- routes/api-route.js
+++ routes/api-route.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts");
 
-// api to get all workouts sorted by date in descending order
+// api to get all workouts sorted by date in ascending order
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
-    .sort({ date: -1 })
+    .sort({ date: 1 })
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
@@ -18,6 +18,7 @@ router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
+      console.log(dbWorkout);
     })
     .catch((err) => {
       res.status(400).json(err);
@@ -30,6 +31,26 @@ router.get("/api/workouts/range", (req, res) => {
       res.json(dbWorkout);
     })
     .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  const workoutId = req.params.id;
+  Workout.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push: { exercise: req.body },
+      $inc: { totalDuration: req.body.duration },
+    },
+    {
+      new: true,
+    }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((errow) => {
       res.status(400).json(err);
     });
 });
