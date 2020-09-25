030a9c664ed6246829a6dc1ddcd81106b071b7e7
MikChanna <channa.mik@gmail.com>
2020-09-25 13:27:12 -0400

    added model to workout schema

---
+++ models/workouts.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  excercise: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter a excercise name",
+      },
+      distance: {
+        type: Number,
+        trim: true,
+        required: "Enter a distance value",
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: "Enter a duration value",
+      },
+      weight: {
+        type: Number,
+        trim: true,
+        required: "Enter a weight value",
+      },
+      sets: {
+        type: Number,
+        trim: true,
+        required: "Enter a value",
+      },
+      reps: {
+        type: Number,
+        trim: true,
+        required: "Enter a value",
+      },
+    },
+  ],
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  totalDuration: {
+    type: Number,
+    trim: true,
+    default: 0,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
